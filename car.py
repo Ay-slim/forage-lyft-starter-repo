f903a561664ff8a38aec5e836d511756b242927d
Ayo <ayooluwaadedipe@gmail.com>
2023-02-06 17:13:32 +0000

    Integrate tire implementation into Car class

--- car.py
+++ car.py
@@ -1,10 +1,12 @@
 from engine import Engine
 from battery import Battery
+from tire import Tire
 
-class Car(Engine, Battery):
-    def __init__(self, engine, battery):
+class Car(Engine, Battery, Tire):
+    def __init__(self, engine, battery, tire):
         self.engine = engine
         self.battery = battery
+        self.tire = tire
     
     def needs_servicing(self):
-        return self.engine.engine_should_be_serviced() or self.battery.battery_should_be_serviced()
+        return self.engine.engine_should_be_serviced() or self.battery.battery_should_be_serviced() or self.tire.tire_should_be_serviced()

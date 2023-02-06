6a41d34edf1f84d312f692055243a3f8dfa24509
Ayo <ayooluwaadedipe@gmail.com>
2023-02-06 12:59:13 +0000

    Pass required arguments

--- engines/sternman.py
+++ engines/sternman.py
@@ -2,7 +2,7 @@ from engine import Engine
 
 class Sternman(Engine):
     def __init__(self, warning_light_is_on):
-        super().__init__(warning_light_is_on)
+        super().__init__(0, 0, warning_light_is_on)
         self.warning_light_is_on = warning_light_is_on
 
     def engine_should_be_serviced(self):

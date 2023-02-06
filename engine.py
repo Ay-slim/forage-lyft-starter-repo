7bebeefc53fbc051e05dbd86de37ecd053ffa431
Ayo <ayooluwaadedipe@gmail.com>
2023-02-06 12:58:58 +0000

    Fix errors found during testing

--- engine.py
+++ engine.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 
 class Engine(ABC):
-  def __init__(self, last_service_mileage, current_mileage, warning_light_is_on):
+  def __init__(self, last_service_mileage, current_mileage, warning_light_is_on=False):
     self.last_service_mileage = last_service_mileage
     self.current_mileage = current_mileage
     self.warning_light_is_on = warning_light_is_on
   
   @abstractmethod
-  def engine_needs_servicing(self):
+  def engine_should_be_serviced(self):
     pass

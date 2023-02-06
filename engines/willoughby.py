b02c656efcc18260706e1d7b277bc05bb6592cef
Ayo <ayooluwaadedipe@gmail.com>
2023-02-06 00:37:21 +0000

    Engines

---
+++ engines/willoughby.py
@@ -0,0 +1,11 @@
+from engine import Engine
+
+
+class Willoughby(Engine):
+    def __init__(self, last_service_mileage, current_mileage):
+        super().__init__(last_service_mileage, current_mileage)
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def engine_should_be_serviced(self):
+        return self.current_mileage - self.last_service_mileage > 60000
